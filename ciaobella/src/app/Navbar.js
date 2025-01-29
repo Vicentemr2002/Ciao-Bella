982c57872b9f7260d212d4c7f944b7a9d1f6f2ad
Jose Vitriago <josevicentevitriago12@gmail.com>
2025-01-29 19:11:01 -0300

    .

---
+++ ciaobella/src/app/Navbar.js
@@ -0,0 +1,33 @@
+'use client'
+import { useState } from 'react';
+import Link from 'next/link';
+import styles from './Navbar.module.css'; // Estilos en CSS Modules
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <header className={styles.header}>
+      <nav className={styles.navbar}>
+        <div className={styles.logo}>Mi Sitio</div>
+        <ul className={`${styles.navLinks} ${isMenuOpen ? styles.active : ''}`}>
+          <li><Link href="/">Inicio</Link></li>
+          <li><Link href="/#services">Servicios</Link></li>
+          <li><Link href="/#about">Nosotros</Link></li>
+          <li><Link href="/#contact">Contacto</Link></li>
+        </ul>
+        <div className={styles.hamburger} onClick={toggleMenu}>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
